0b752f7910fd3b8d20107503670289cc256e1f9e
hellodude <vaman852@gmail.com>
2020-05-05 00:53:59 +0530

    Updates

---
+++ precache-manifest.cd96297edf002f825206bc5f5ac96f08.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51b9227801cffff8b4a4fbbfe1bebfb8",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "930419f4ebc8688b6bff",
+    "url": "/react-deploy/static/css/main.2a11f4d4.chunk.css"
+  },
+  {
+    "revision": "a0807612eceab01ab3c1",
+    "url": "/react-deploy/static/js/2.b12c7bd2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-deploy/static/js/2.b12c7bd2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "930419f4ebc8688b6bff",
+    "url": "/react-deploy/static/js/main.3ba443bc.chunk.js"
+  },
+  {
+    "revision": "4d275ecffff8f8311b08",
+    "url": "/react-deploy/static/js/runtime-main.8e4be4b4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
